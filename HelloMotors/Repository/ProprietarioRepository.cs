139dce2e9ac4995ba9817f7923d65aff3e3beb7d
potatsts <yeppotatinhas@gmail.com>
2026-02-08 00:54:49 -0300

    Padrão Repository do Proprietario
    criação do repositorio e serviço da entidade proprietario para utilização no controller

---
+++ HelloMotors/Repository/ProprietarioRepository.cs
@@ -0,0 +1,17 @@
+using HelloMotors.Data;
+
+namespace HelloMotors.Repository;
+
+public class ProprietarioRepository
+{
+    private AppDbContext _context;
+    public ProprietarioRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    //listar todos
+    //cadastrar
+    //atualizar
+    //deletar
+}
